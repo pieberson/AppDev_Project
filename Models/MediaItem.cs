a2b819d0e13ce901ab9e59dce26805179a4e2f39
jennqmae <114900728+jennq-m@users.noreply.github.com>
2025-01-12 16:13:54 +0800

    Create MediaItem.cs

---
+++ Models/MediaItem.cs
@@ -0,0 +1,11 @@
+namespace AppDev.Models
+{
+    public class MediaItem
+    {
+        public string Class { get; set; }
+        public string Title { get; set; }
+        public int YearFinished { get; set; }
+        public int Rating { get; set; }
+        public string? Review { get; set; }
+    }
+}
